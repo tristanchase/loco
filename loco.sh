522bd34c16934ae29d500bcdd13621d5af03f1b2
Tristan Chase <tristan.m.chase@gmail.com>
2016-09-18 21:01:08 -0400

    Not sure what I changed here...

--- loco.sh
+++ loco.sh
@@ -6,10 +6,13 @@
 # A script called row (which see) will work from within this script.
 # A script called filetype appends a character to the end of each line 
 # (like ls -F).  I would like to merge it inside this script.
+
+# [Explain what this does.]
 outfile=/tmp/loco.$$
 touch ${outfile}
 export outfile
 
+# [Explain what this does.]
 trap cleanup EXIT
 
 cleanup () {
@@ -23,7 +26,10 @@ awk_statement () {
 	$cmd $obj
 }
 
+# [Explain what this does.]
 count=$(locate -c "$1")
+
+# [Handle warning about locate database being over 8 days old]
 
 if [ $count -gt "10000" ]; then
 	echo -n "File will have "$count" lines.  Do you wish to continue?(y/N): "; read response
@@ -40,6 +46,7 @@ if [ $count -gt "10000" ]; then
 	esac
 fi
 
+# [Explain what this does.]
 locate -i -0 "$1" | xargs -0 filetype |  sed = | sed 'N;s/\n/\t/' > ${outfile}
 
 # This line adds backslash escapes to the filenames containing spaces.
