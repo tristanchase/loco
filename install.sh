7088c99ac6c2ce7fa9d80cb08ccb8ef8fffd48e6
Tristan Chase <tristan.m.chase@gmail.com>
2016-09-18 21:37:18 -0400

    Outline process

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@
 # Variables
 
 ## Dependencies
+
 ### System
 sys_deps="locate updatedb xargs"
 
@@ -15,8 +16,23 @@ files="loco row filetype"
 dir=$HOME/bin
 
 # Process
-## Test sys_deps
+
+## Test $sys_deps
+
 ### If any are missing, prompt for installation
-### Install missing sys_deps
+
+### Install missing $sys_deps
+
+## Download raw $files from GitHub to $dir
+
+## Rename the $files (drop the .sh)
+
+## Make the $files executable
+
+## Check to see if $dir is in $PATH
+
+### If not, add it and modify .(bas|zs|oh-my-zs)hrc to include it.
+
+## Make sure your locate database is up to date
 
 
